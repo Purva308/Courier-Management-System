81c2015ca08a7b19aba73510edb26a92d8765b38
Purva <purva3082003@gmail.com>
2025-07-02 14:30:47 +0530

    Initial Commit

--- entity/courier.py
+++ entity/courier.py
@@ -1,5 +1,6 @@
 class Courier:
-    def __init__(self,courier_id=None,sender_name="",sender_address="",receiver_name="",receiver_address="",weight="",status="",tracking_number="",deilevery_date="",pickup_time="",employee_id="",location_id="",service_id=""):
+    tracking_id_seq = 10000
+    def __init__(self,courier_id=None,sender_name="",sender_address="",receiver_name="",receiver_address="",weight=0.0,status="",deilevery_date=None,pickup_time=None,employee_id="",location_id="",service_id=""):
         self.__courier_id=courier_id
         self.__sender_name=sender_name
         self.__sender_address=sender_address
@@ -7,8 +8,9 @@ class Courier:
         self.__receiver_address=receiver_address
         self.__weight=weight
         self.__status=status
-        self.__tracking_number=tracking_number
-        self.__delivery_date=deilevery_date
+        self.__tracking_number=f"T{Courier.tracking_id_seq}"
+        Courier.tracking_id_seq += 1
+        self.__deilevery_date=deilevery_date
         self.__pickup_time=pickup_time
         self.__employee_id=employee_id
         self.__location_id=location_id
@@ -43,4 +45,32 @@ class Courier:
         return self.__service_id
 
     #Setter
+    def courier_id(self,value):
+        self.__courier_id=value
+    def sender_name(self,value):
+        self.__sender_name=value
+    def sender_address(self,value):
+        self.__sender_address=value
+    def receiver_name(self,value):
+        self.__receiver_name=value
+    def receiver_address(self,value):
+        self.__receiver_address=value
+    def weight(self,value):
+        self.__weight=value
+    def status(self,value):
+        self.__status=value
+    def tracking_number(self,value):
+        self.__tracking_number=value
+    def deilevery_date(self,value):
+        self.__deilevery_date=value
+    def pickup_time(self,value):
+        self.__pickup_time=value
+    def employee_id(self,value):
+        self.__employee_id=value
+    def location_id(self,value):
+        self.__location_id=value
+    def service_id(self,value):
+        self.__service_id=value
+
+
 
